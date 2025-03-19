b4cae1f0caa58102f8d7f4a5c046ccd7434bf354
davixxx001 <davixgmm7@gmail.com>
2025-03-19 13:01:52 +0100

    adding PrintMethod.java

---
+++ PrintMethod.java
@@ -0,0 +1,18 @@
+public class PrintMethod{
+	public static void main(String[] args) {
+		//using the print()
+		System.out.print("\"learning java is fun\"\n");
+		System.out.print(" hey, come and learn java with me");
+		
+		//using the println()
+		System.out.println();
+		System.out.println("i want to tell you that java is different from java script");
+		System.out.println("\t this is the indented text");
+		
+		// using the printf()
+		System.out.printf("My name is %s and i love %s. Good day everyone%n","David","Java");
+		System.out.printf("I am %d years old",25);
+		System.out.printf("I bought JDK for %.2f naira%n",159.3521);
+		System.out.printf("I bought JDK for %c%.2f %n",'$',159.3521);
+	}
+}
