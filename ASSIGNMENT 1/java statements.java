190bfd5f940397057a270b5e7daaa6cc443cb0ce
davixxx001 <davixgmm7@gmail.com>
2025-03-24 00:44:45 +0100

    Adding Arithmetic operations

---
+++ ASSIGNMENT 1/java statements.java
@@ -0,0 +1,23 @@
+//creating an integer calculator
+// creating a scanner
+Scanner input = new
+scanner(system.in;
+//Declaring variables
+int x,a,z result;
+// input first integer
+System.out.print("Enter your First integer")
+x = input.nextInt();
+
+//input second integer
+System.out.print("Enter your First integer")
+a = input.nextInt();
+
+////input Third integer
+System.out.print("Enter your First integer")
+z = input.nextIn()
+
+//product
+result = x*a*z
+
+//Display result
+System.out.printf("Product is %d%n",result);
---
+++ ASSIGNMENT 1/java statements2.java
@@ -0,0 +1,6 @@
+//a)Display message with cursor on same line
+System.out.print("enter an integer:");
+//b) Assign product of b and c to a 
+a = b*c;
+//c) Comment explaining a sample payroll calculation
+// this program calculates payroll for employees.
